62074047faca0be750db217f1fff8bd18e7475ad
JoanChirinos <jchirinos@stuy.edu>
2018-08-21 18:53:40 -0500

    .

---
+++ makeTally.py
@@ -0,0 +1,70 @@
+#! /usr/bin/python
+
+import cgi, cgitb
+
+cgitb.enable()
+
+print 'Content-type: text/html\n\n'
+
+def go():
+    fs = cgi.FieldStorage()
+    choices = []
+    
+    for i in fs.keys():
+        choices += [fs.getvalue(i)]
+    
+    top = """<!doctype html>
+    <html lang="en">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <title>Your Tally Counter</title>
+    </head>
+
+    <body>
+        <div class="contaier">
+    """
+    
+    bottom = """
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+    </html>"""
+    
+    toAdd = ''
+    btntypes = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'lighht', 'dark']
+    on = -1
+    for i in choices:
+        on += 1
+        on %= 8
+        btn = btntypes[on]
+        toAdd += '<div class="row"><div class="col"><button type="button" class="btn btn-' + btn + '" onclick="buttonClick("' + i + '")>' + i + '</button>%nbsp;%nbsp;%nbsp;%nbsp;<div class="inline-block" id="' + i + '">0</div>\n\n\n'
+    print top
+    print toAdd
+    print bottom
+
+go()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ script.js
@@ -0,0 +1,28 @@
+//var doc = document.getElementById('inputArea');
+
+//
+
+function checkNumberOfChoices() {
+    var num = document.getElementById('numberOfChoices').value;
+    if (num == "") {
+        return;
+    }
+    var x = parseInt(num);
+    document.getElementById('numberOfChoices').value = Math.abs(Math.floor(x));
+}
+
+function enteredChoices() {
+    var doc = document.getElementById('inputArea');
+    var num = parseInt(document.getElementById('numberOfChoices').value);
+    doc.innerHTML = "Loading...";
+    if (num < 1) {
+        num = 1;
+    }
+    var toPlaceInInputArea = '<form action="makeTally.py" method="GET" target="_blank">'
+    for (var i = 0; i < num; i++) {
+        toPlaceInInputArea += '<div class="row"><div class="col"><div class="form-group"><label for="choice' + (i + 1) + '"> Choice ' + (i + 1) + '</label><input type="text" id="choice' + (i + 1) + '" class="form-control" name="choice' + (i + 1) + '"></div></div></div>';
+    }
+    toPlaceInInputArea += '<div class="row"><div class="col"><input type="submit" class="btn btn-success" name="Next" value="Next"></div></div></form>';
+    console.log(toPlaceInInputArea);
+    doc.innerHTML = toPlaceInInputArea;
+}
